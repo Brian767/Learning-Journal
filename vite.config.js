6ead61856cc558d1c11787b7aab3e6f8a0ab6f3d
Brian767 <brra7685@colorado.edu>
2026-05-27 00:29:45 -0600

    text file path 5

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import {defineConfig} from "vite"
+import { dirname, resolve } from "node:path"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-	plugins: [
-		
-	]
+  base: "/",
+  build: {
+    rolldownOptions: {
+      input: {
+        main: resolve(import.meta.dirname, "index.html"),
+        about: resolve(import.meta.dirname, "pages/about.html"),
+        hero: resolve(import.meta.dirname, "pages/hero.html"),
+      },
+    },
+  },
+  plugins: []
 })
